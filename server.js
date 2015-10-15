2a1d00bc6a95e5828105463142b7228f1a2157e3
balaramantelecmi <balaraman993@gmail.com>
2015-10-15 19:10:54 +0530

    cc

---
+++ server.js
@@ -0,0 +1,19 @@
+var express=require('express'),
+   app=express(),
+   http=require('http'),
+   server=http.createServer(app);
+
+
+
+
+
+  app.get('/',function(req,res){
+
+    res.send('hai   ');  });
+
+
+
+
+ server.listen(1235);
+
+
